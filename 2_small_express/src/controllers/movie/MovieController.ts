abf6e7af110ee7b906ad7fe3c0639d28416202b1
Muhammad Husein Nashr <nvdhunter007@gmail.com>
2021-10-08 07:12:14 +0700

    get movie

--- 2_small_express/src/controllers/movie/MovieController.ts
+++ 2_small_express/src/controllers/movie/MovieController.ts
@@ -1,5 +1,6 @@
 import { JsonController, Get, QueryParams } from 'routing-controllers';
-import { OmdbService } from '../../services/OmdbService';
+import { DataTypeEnum, OmdbService } from '../../services/OmdbService';
+import { GetMovieQuery } from './definition/GetMovieQuery';
 import { SearcMovieQuery } from './definition/SearchMovieQuery';
 
 const omdbService = new OmdbService(process.env.OMDB_API_URL, process.env.OMDB_API_KEY);
@@ -9,7 +10,7 @@ export class MovieController {
   @Get('/search')
   async search(@QueryParams() searchMovieQueries: SearcMovieQuery) {
     const [searchRes, searchErr] = await omdbService.search({ 
-      s: searchMovieQueries.title, page: searchMovieQueries.page, type: searchMovieQueries.type, y: searchMovieQueries.year, r: "json" 
+      s: searchMovieQueries.title, page: searchMovieQueries.page, type: searchMovieQueries.type, y: searchMovieQueries.year, r: DataTypeEnum.json
     })
     if(searchErr) {
       if(searchErr.isClient) return { data: null, error: { message: searchErr.message }};
@@ -19,5 +20,20 @@ export class MovieController {
     }
 
     return { data: searchRes, error: null }
+  }
+
+  @Get('/get')
+  async getByIdOrTitle(@QueryParams() getMoviewQueries: GetMovieQuery) {
+    const [getRes, getErr] = await omdbService.get({ 
+      i: getMoviewQueries.id, t: getMoviewQueries.title, plot: getMoviewQueries.plot, type: getMoviewQueries.type, y: getMoviewQueries.year, r: DataTypeEnum.json 
+    })
+    if(getErr) {
+      if(getErr.isClient) return { data: null, error: { message: getErr.message }};
+
+      console.error("MovieController::getByIdOrTitle", getErr.message)
+      return { data: null, error: { message: "Something when wrong. Please try again later" }}
+    }
+
+    return { data: getRes, error: null }
   }
 }
